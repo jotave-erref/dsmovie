7b8f8545152cc7aaca91c88bdfdf71de4971a9ab
Jean Victor <jvrferreira96@gmail.com>
2022-01-12 14:12:48 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import {ReactComponent as GitHubIcon} from './assets/img/github.svg';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <NavBar/>
   );
 }
 
