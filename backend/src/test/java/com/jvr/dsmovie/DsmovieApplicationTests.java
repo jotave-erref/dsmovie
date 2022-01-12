a23f8790ba9680670b68cd0eb945eac6837a89b4
Jean Victor <jvrferreira96@gmail.com>
2022-01-12 10:39:10 -0300

    Project created

---
+++ backend/src/test/java/com/jvr/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvr.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
